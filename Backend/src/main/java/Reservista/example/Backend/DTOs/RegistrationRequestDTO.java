05f1e8658d9a92bb486b347feb544082aa4b147e
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2023-11-30 15:55:49 +0200

    Add files

---
+++ Backend/src/main/java/Reservista/example/Backend/DTOs/RegistrationRequestDTO.java
@@ -0,0 +1,35 @@
+package Reservista.example.Backend.DTOs;
+
+import Reservista.example.Backend.Validators.Gmail;
+import Reservista.example.Backend.Validators.StrongPassword;
+import Reservista.example.Backend.Validators.Username;
+import Reservista.example.Backend.Validators.ValidBirthDate;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+@Builder
+public class RegistrationRequestDTO {
+
+    @Gmail
+    String email;
+
+    @StrongPassword
+    String password;
+
+    @Username
+    String userName;
+
+    @NotBlank(message = "please provide your first name")
+    String firstName;
+
+    String lastName;
+
+   @ValidBirthDate
+   LocalDate birthDate;
+
+}
---
+++ Backend/src/main/java/Reservista/example/Backend/DTOs/RegistrationResponseDTO.java
@@ -0,0 +1,31 @@
+package Reservista.example.Backend.DTOs;
+
+import Reservista.example.Backend.Validators.Gmail;
+import Reservista.example.Backend.Validators.StrongPassword;
+import Reservista.example.Backend.Validators.Username;
+import Reservista.example.Backend.Validators.ValidBirthDate;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class RegistrationResponseDTO {
+
+    private String email;
+
+    private String password;
+
+    private String userName;
+
+    private String firstName;
+
+    private String lastName;
+
+//    private String birthDate;
+
+    private String response;
+
+}
