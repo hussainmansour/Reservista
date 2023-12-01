abe1a3a8a70a1e2393d0bce91a5493aeda4c6f4d
MohamedAnwar121 <mohamedabdalrahman6@gmail.com>
2023-12-01 16:26:45 +0200

    Modifying jpa entities to add missing attributes

--- Backend/src/main/java/Reservista/example/Backend/Controllers/ConfigController.java
+++ Backend/src/main/java/Reservista/example/Backend/Controllers/ConfigController.java
@@ -1,6 +1,7 @@
 package Reservista.example.Backend.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class ConfigController {
     private Set<String> validCountries;
 
     @GetMapping("/countries")
-    public Set<String> getValidCountries() {
-        return validCountries;
+    public ResponseEntity<Set<String>> getValidCountries() {
+        return ResponseEntity.ok(validCountries);
     }
 }
