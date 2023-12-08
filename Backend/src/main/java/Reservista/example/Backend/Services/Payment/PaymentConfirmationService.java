2679ee36c4fd8121c68789f1a36efbde49a1d233
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2023-12-08 07:03:33 +0200

    Stripe web hook
    - autorun configuration is required to be able to redirect stripe events to out localhosts
    - incomplete PaymentConfirmationService

---
+++ Backend/src/main/java/Reservista/example/Backend/Services/Payment/PaymentConfirmationService.java
@@ -0,0 +1,26 @@
+package Reservista.example.Backend.Services.Payment;
+
+import com.stripe.exception.EventDataObjectDeserializationException;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.model.PaymentIntent;
+import com.stripe.net.ApiResource;
+import com.stripe.net.Webhook;
+import org.hibernate.annotations.SecondaryRow;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentConfirmationService {
+
+
+    public void confirmPayment(Event event) throws EventDataObjectDeserializationException {
+
+            PaymentIntent paymentIntent = ApiResource.GSON.fromJson(event.getDataObjectDeserializer().deserializeUnsafe().toJson(), PaymentIntent.class);
+            String paymentIntentId = paymentIntent.getId();
+
+            // confirm the reservation ( I will need to save the paymentIntentID )
+   
+    }
+}
