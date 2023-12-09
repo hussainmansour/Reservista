b333455fb9d2e4d3d194eed374752bd271e23b90
mariamgerges575 <96488115+mariamgerges575@users.noreply.github.com>
2023-12-09 14:43:28 +0200

    Calculation handler done without testing

--- Backend/src/main/java/Reservista/example/Backend/DTOs/InvoiceComponent/ReservationDTO.java
+++ Backend/src/main/java/Reservista/example/Backend/DTOs/InvoiceComponent/ReservationDTO.java
@@ -47,31 +47,26 @@ public class ReservationDTO {
     @BooleanFlag
     private boolean refundable;
 
-    private String voucherCode;
+    private double refundAdditionalPercentage;
 
+    private String voucherCode;
     private double voucherPercentage;
 
     private double price;
+    private double finalPrice;
 
-
-    public Pair<Double, String> calculate_price() {
-        double total = 0;
-        StringBuilder s = new StringBuilder();
-        s.append("Your Rooms:\n");
-        for (int i = 0; i < rooms.size(); i++) {
-            Pair<Double, String> priceAndDetails = rooms.get(i).calculate_price();
-            total += priceAndDetails.getFirst();
-            s.append(priceAndDetails.getSecond());
-        }
-        int intRefundable = this.refundable ? 1 : 0;
-        total += Math.ceil(0.13 * intRefundable * total);
-        if (isRefundable()) s.append("\nRefundable\t\t\t" + "13%(+").append(total * 0.13).append(")\n\n");
-        if (voucherPercentage != 0)
-            s.append("\nVoucher\t\t\t").append(voucherPercentage * 100).append("%(-+").append(total * voucherPercentage).append(")\n");
-        total -= total * voucherPercentage;
-        s.append("Total \t\t\t").append(total).append("\n\n");
-
-        return Pair.of(total, s.toString());
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder("Your Rooms:\n");
+        for(RoomDTO r :rooms)
+            s.append(r.toString());
+        s.append("\n");
+        if(refundable)
+            s.append("\nRefundable additional percentage: + ").append(refundAdditionalPercentage*100).append("%\n");
+        if(voucherCode!=null)
+            s.append("\nVoucher: -").append(voucherPercentage * 100).append("%\n");
+        s.append("Final price: ").append(finalPrice);
+        return s.toString();
     }
 }
 
---
+++ Backend/src/main/java/Reservista/example/Backend/Services/Reservation/CalculationHandler.java
@@ -0,0 +1,32 @@
+package Reservista.example.Backend.Services.Reservation;
+
+import Reservista.example.Backend.DTOs.InvoiceComponent.OptionDTO;
+import Reservista.example.Backend.DTOs.InvoiceComponent.ReservationDTO;
+import Reservista.example.Backend.DTOs.InvoiceComponent.RoomDTO;
+import Reservista.example.Backend.DTOs.Response.ReservationResponseDTO;
+import Reservista.example.Backend.DTOs.Response.ResponseDTO;
+
+public class CalculationHandler extends ReservationHandler{
+    public  ResponseDTO<ReservationResponseDTO> handleRequest(ReservationDTO reservationDTO){
+        calculate_price(reservationDTO);
+        return nextHandler.handleRequest(reservationDTO);
+    }
+    public void calculate_price(ReservationDTO reservationDTO) {
+        double total = 0;
+        for(RoomDTO r:reservationDTO.getRooms()){
+            double optionPrice=0;
+            if(r.getOptions()!=null ) {
+                for (OptionDTO o : r.getOptions()) {
+                    optionPrice+=o.getPrice();
+                }
+            }
+            r.setOptionsTotalPrice(optionPrice);
+            total+= total+optionPrice;
+        }
+        reservationDTO.setPrice(total);
+        total+=total* reservationDTO.getRefundAdditionalPercentage();
+        total-=total* reservationDTO.getVoucherPercentage();
+
+        reservationDTO.setFinalPrice(total);
+    }
+}
