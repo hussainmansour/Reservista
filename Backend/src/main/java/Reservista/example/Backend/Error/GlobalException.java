776c569cb42a292cb9dc5e63c9085858d215d4c8
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2023-12-27 22:28:16 +0200

    removing comments

--- Backend/src/main/java/Reservista/example/Backend/Error/GlobalException.java
+++ Backend/src/main/java/Reservista/example/Backend/Error/GlobalException.java
@@ -6,13 +6,11 @@ import org.springframework.http.HttpStatus;
 
 @Data
 public class GlobalException extends Exception{
-
-//    private int code;
+    
     private HttpStatus httpStatus;
     private ErrorCode errorCode;
 
     public GlobalException(ErrorCode errorCode, HttpStatus httpStatus){
-//        this.code=errorCode.getCode();
         super();
         this.errorCode = errorCode;
         this.httpStatus=httpStatus;
