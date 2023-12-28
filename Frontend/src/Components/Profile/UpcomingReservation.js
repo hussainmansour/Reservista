78e2c76efb062c8b37c6764834305b1d481ec181
husseinkhaled733 <husseinkhaled733@gmail.com>
2023-12-28 03:55:47 +0200

    Uncommenting some code

--- Frontend/src/Components/Profile/UpcomingReservation.js
+++ Frontend/src/Components/Profile/UpcomingReservation.js
@@ -11,16 +11,16 @@ import { CancellationAPI } from '../../Utilities/New/APIs/CancellationAPI';
 
 const UpcomingReservation = () => {
 
-  const reservationData = [
-    { reservationID: '1', hotelName: 'Hotel A', roomTitle: 'Single Room', reservationDate: '2023-01-01', checkIn: '2023-02-01', checkOut: '2023-02-10', noOfRooms: 2, price: 200, invoice: "Reservation Details:\nReservation ID: 3\nCheck-in Date: 2024-01-04T12:00:00Z\nCheck-out Date: 2024-01-10T12:00:00Z\nRoom Details:\n\nStandard Room, 1 King Bed\nRoom price: 227\nnumber of rooms reserved: 3\nRoom prices after optional services\nRoom1: 300\t+Lunch: 73\n\t+Dinner: 141\nRoom prices after optional services\nRoom2: 300\t+Lunch: 73\n\t+Dinner: 141\nRoom prices after optional services\nRoom3: 300\t+Lunch: 73\n\t+Dinner: 141\nAdditional percentage due to refund option: +0%\nTotal price: 900" },
-    { reservationID: '2', hotelName: 'Hotel B', roomTitle: 'Double Room', reservationDate: '2023-02-01', checkIn: '2023-03-01', checkOut: '2023-03-10', noOfRooms: 1, price: 150, invoice: 'Invoice B' },
-    { reservationID: '3', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
-    { reservationID: '4', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
-    { reservationID: '5', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
-    { reservationID: '6', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
-    { reservationID: '7', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
-    // Add more reservation data items as needed
-  ];
+  // const reservationData = [
+  //   { reservationID: '1', hotelName: 'Hotel A', roomTitle: 'Single Room', reservationDate: '2023-01-01', checkIn: '2023-02-01', checkOut: '2023-02-10', noOfRooms: 2, price: 200, invoice: "Reservation Details:\nReservation ID: 3\nCheck-in Date: 2024-01-04T12:00:00Z\nCheck-out Date: 2024-01-10T12:00:00Z\nRoom Details:\n\nStandard Room, 1 King Bed\nRoom price: 227\nnumber of rooms reserved: 3\nRoom prices after optional services\nRoom1: 300\t+Lunch: 73\n\t+Dinner: 141\nRoom prices after optional services\nRoom2: 300\t+Lunch: 73\n\t+Dinner: 141\nRoom prices after optional services\nRoom3: 300\t+Lunch: 73\n\t+Dinner: 141\nAdditional percentage due to refund option: +0%\nTotal price: 900" },
+  //   { reservationID: '2', hotelName: 'Hotel B', roomTitle: 'Double Room', reservationDate: '2023-02-01', checkIn: '2023-03-01', checkOut: '2023-03-10', noOfRooms: 1, price: 150, invoice: 'Invoice B' },
+  //   { reservationID: '3', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
+  //   { reservationID: '4', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
+  //   { reservationID: '5', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
+  //   { reservationID: '6', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
+  //   { reservationID: '7', hotelName: 'Hotel C', roomTitle: 'Triple Room', reservationDate: '2023-03-01', checkIn: '2023-04-01', checkOut: '2023-04-10', noOfRooms: 3, price: 300, invoice: 'Invoice C' },
+  //   // Add more reservation data items as needed
+  // ];
 
   function getReservationById(reservationID) {
     return reservationData.find(reservation => reservation.reservationID === reservationID);
@@ -34,50 +34,50 @@ const UpcomingReservation = () => {
   const [details, setDetails] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  // // Fetch data
-  // const [reservationData, setReservationData] = useState([]);
-  // useEffect(()=>{
-  //   const fetchReservations=async()=>{
-  //     let response = await ProfileAPI.getUpcomingReservation((response)=>{
-  //       const responseBody=response.data;
-  //       if(responseBody.data!==undefined){
-  //         console.log(responseBody.data);
-  //         Alert.alert('Error',responseBody.data);
-  //       }
-  //       else{
-  //         console.log('====================================');
-  //         console.log(responseBody);
-  //         console.log('====================================');
-  //         Alert.alert("Error", "Failed to get your reservations");
-  //       }
-  //     },setIsLoading);
+  // Fetch data
+  const [reservationData, setReservationData] = useState([]);
+  useEffect(()=>{
+    const fetchReservations=async()=>{
+      let response = await ProfileAPI.getUpcomingReservation((response)=>{
+        const responseBody=response.data;
+        if(responseBody.data!==undefined){
+          console.log(responseBody.data);
+          Alert.alert('Error',responseBody.data);
+        }
+        else{
+          console.log('====================================');
+          console.log(responseBody);
+          console.log('====================================');
+          Alert.alert("Error", "Failed to get your reservations");
+        }
+      },setIsLoading);
 
-  //     if(response!==undefined){
-  //       console.log('====================================');
-  //       console.log(response);
-  //       console.log('====================================');
-  //       setReservationData(response);
-  //     }
-  //   };
-  //   fetchReservations();
-  // },[]);
+      if(response!==undefined){
+        console.log('====================================');
+        console.log(response);
+        console.log('====================================');
+        setReservationData(response);
+      }
+    };
+    fetchReservations();
+  },[]);
 
 
-  // // Wait until data is loaded before rendering
-  // if (isLoading) {
-  //   return (
-  //     <LoadingComponent></LoadingComponent>
-  //   );
-  // }
+  // Wait until data is loaded before rendering
+  if (isLoading) {
+    return (
+      <LoadingComponent></LoadingComponent>
+    );
+  }
 
-  // // check if there is no reservation
-  // if(reservationData.length==0){
-  //   return(
-  //     <View style={styles.container}>
-  //       <Text style={{...cardStyles.Title,fontSize:20}}>You have no upcoming reservations</Text>
-  //     </View>
-  //   );
-  // }
+  // check if there is no reservation
+  if(reservationData.length==0){
+    return(
+      <View style={styles.container}>
+        <Text style={{...cardStyles.Title,fontSize:20}}>You have no upcoming reservations</Text>
+      </View>
+    );
+  }
 
 
 
